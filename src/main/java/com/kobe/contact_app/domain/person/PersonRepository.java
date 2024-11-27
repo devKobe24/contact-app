3d964afc50fda87c4c64b75fc2688beebe76610b
Kobe <dev.skyachieve91@gmail.com>
2024-11-28 08:17:27 +0900

    성으로 사람 검색하는 추상 메서드 구현.

--- src/main/java/com/kobe/contact_app/domain/person/PersonRepository.java
+++ src/main/java/com/kobe/contact_app/domain/person/PersonRepository.java
@@ -1,10 +1,16 @@
 package com.kobe.contact_app.domain.person;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
     // 이름으로 찾는 메서드.
     List<Person> findByName(String name);
+
+    // 성으로 찾는 메서드.
+    @Query("SELECT p FROM Person p WHERE p.name LIKE %:name%")
+    List<Person> findByPartialName(@Param("name") String name);
 }
