bebb69aec5644e0c2cfb2373b6c5d8ca3d85e560
Kobe <dev.skyachieve91@gmail.com>
2024-11-27 14:09:22 +0900

    PersonController 생성 및 구현, POST(연락처 저장), GET(이름으로 검색) 구현.

---
+++ src/main/java/com/kobe/contact_app/controller/PersonController.java
@@ -0,0 +1,30 @@
+package com.kobe.contact_app.controller;
+
+import com.kobe.contact_app.dto.person.request.PersonCreateRequest;
+import com.kobe.contact_app.dto.person.response.PersonResponse;
+import com.kobe.contact_app.service.person.PersonService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class PersonController {
+    private final PersonService personService;
+
+    public PersonController(PersonService personService) {
+        this.personService = personService;
+    }
+
+    @PostMapping("/save")
+    public void savePerson(@RequestBody PersonCreateRequest request) {
+        personService.savePerson(request);
+    }
+
+    @GetMapping("/search")
+    public List<PersonResponse> getPeopleByFullName(String name) {
+        return personService.getPeopleByFullName(name);
+    }
+}
