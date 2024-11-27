88ebb6e6e8c09eca775c56ffba3987c753198ca7
Kobe <dev.skyachieve91@gmail.com>
2024-11-27 14:46:51 +0900

    동명이인이 있을 수 있으므로 Optional에서 List로 타입 변경.

--- src/main/java/com/kobe/contact_app/domain/person/PersonRepository.java
+++ src/main/java/com/kobe/contact_app/domain/person/PersonRepository.java
@@ -2,9 +2,9 @@ package com.kobe.contact_app.domain.person;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
     // 이름으로 찾는 메서드.
-    Optional<Person> findByName(String name);
+    List<Person> findByName(String name);
 }
