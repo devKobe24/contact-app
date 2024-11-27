488c0f98aa0d8ed017487823e08de7686eb3f9e3
Kobe <dev.skyachieve91@gmail.com>
2024-11-28 08:59:08 +0900

    findByName 추상 메서드 삭제, Query문 수정.

--- src/main/java/com/kobe/contact_app/domain/person/PersonRepository.java
+++ src/main/java/com/kobe/contact_app/domain/person/PersonRepository.java
@@ -7,10 +7,6 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
-    // 이름으로 찾는 메서드.
-    List<Person> findByName(String name);
-
-    // 성으로 찾는 메서드.
-    @Query("SELECT p FROM Person p WHERE p.name LIKE %:name%")
+    @Query("SELECT p FROM Person p WHERE p.firstName LIKE %:name% OR p.lastName LIKE %:name%")
     List<Person> findByPartialName(@Param("name") String name);
 }
