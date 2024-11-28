9ab47ba43b583efd1b99e76c24e3165fbee17628
Kobe <dev.skyachieve91@gmail.com>
2024-11-29 08:33:56 +0900

    @Modify, @Transactional 추가

--- src/main/java/com/kobe/contact_app/domain/person/PersonRepository.java
+++ src/main/java/com/kobe/contact_app/domain/person/PersonRepository.java
@@ -1,8 +1,10 @@
 package com.kobe.contact_app.domain.person;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
