fa9dc33c8fce1caadeb3fb4b13f66861d060285d
Kobe <dev.skyachieve91@gmail.com>
2024-11-28 09:32:42 +0900

    getPeopleByPhoneNumber 메서드 생성 및 구현.

--- src/main/java/com/kobe/contact_app/controller/PersonController.java
+++ src/main/java/com/kobe/contact_app/controller/PersonController.java
@@ -3,10 +3,7 @@ package com.kobe.contact_app.controller;
 import com.kobe.contact_app.dto.person.request.PersonCreateRequest;
 import com.kobe.contact_app.dto.person.response.PersonResponse;
 import com.kobe.contact_app.service.person.PersonService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +23,10 @@ public class PersonController {
     @GetMapping("/search")
     public List<PersonResponse> getPeopleByName(String name) {
         return personService.getPeopleByName(name);
+    }
+
+    @GetMapping("/search/phone")
+    public List<PersonResponse> getPeopleByPhoneNumber(@RequestParam("number") String phoneNumber) {
+        return personService.getPeopleByPhoneNumber(phoneNumber);
     }
 }
