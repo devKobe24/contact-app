350bd439bc90edd4d22325ab13e94db90a32dec4
Kobe <dev.skyachieve91@gmail.com>
2024-11-29 08:34:32 +0900

    updateByPhoneNumber 메서드 생성 및 구현.

--- src/main/java/com/kobe/contact_app/service/person/PersonService.java
+++ src/main/java/com/kobe/contact_app/service/person/PersonService.java
@@ -51,4 +51,20 @@ public class PersonService {
                 .map(PersonResponse::new)
                 .collect(Collectors.toList());
     }
+
+    @Transactional
+    public PersonResponse updateByPhoneNumber(Long id, String phoneNumber) {
+        // Update 쿼리 실행.
+        int updatedCount = personRepository.updateByPhoneNumber(id, phoneNumber);
+
+        // 업데이트가 실패한 경우 예외 처리
+        if (updatedCount == 0) {
+            throw new IllegalArgumentException("No person found with ID: " + id);
+        }
+
+        // 업데이트된 데이터를 다시 조회
+        return personRepository.findById(id)
+                .map(PersonResponse::new)
+                .orElseThrow(() -> new IllegalArgumentException("No person found with ID: " + id));
+    }
 }
