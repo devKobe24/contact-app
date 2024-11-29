ffbf18479f8d034585696e1fa7953616e63967c8
Kobe <dev.skyachieve91@gmail.com>
2024-11-29 09:57:00 +0900

    PersonEmailUpdateRequest dto 클래스 생성 및 구현.

---
+++ src/main/java/com/kobe/contact_app/dto/person/request/PersonEmailUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.kobe.contact_app.dto.person.request;
+
+public class PersonEmailUpdateRequest {
+    private Long id;
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+}
