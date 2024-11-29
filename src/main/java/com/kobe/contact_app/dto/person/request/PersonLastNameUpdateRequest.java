88af6adcc77425b5fd75a22cee5a76d6dfe7c024
Kobe <dev.skyachieve91@gmail.com>
2024-11-29 10:29:19 +0900

    PersonLastNameUpdateRequest dto 클래스 생성 및 구현.

---
+++ src/main/java/com/kobe/contact_app/dto/person/request/PersonLastNameUpdateRequest.java
@@ -0,0 +1,14 @@
+package com.kobe.contact_app.dto.person.request;
+
+public class PersonLastNameUpdateRequest {
+    private Long id;
+    private String lastName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
