f6f539e784644a01736a8bf28788fcf85c4f89f2
Kobe <dev.skyachieve91@gmail.com>
2024-12-02 12:15:46 +0900

    1. import 된 사용하지 않는 PersonCallback, ContactState, NameState, MissingFieldException 삭제. 2. NameStateProcessor util 클래스의 static 메서드(클래스 메서드) determineFirstNameAndLastNameState 구현. 3. determineFirstNameAndLastNameState, processNameState, determineNameState, determineContactState를 모듈화하기 위하여 전체 삭제.

--- src/main/java/com/kobe/contact_app/dto/person/request/PersonCreateRequest.java
+++ src/main/java/com/kobe/contact_app/dto/person/request/PersonCreateRequest.java
@@ -1,9 +1,6 @@
 package com.kobe.contact_app.dto.person.request;
 
-import com.kobe.contact_app.callback.PersonCallback;
-import com.kobe.contact_app.constant.ContactState;
-import com.kobe.contact_app.constant.NameState;
-import com.kobe.contact_app.exception.MissingFieldException;
+import com.kobe.contact_app.util.name.NameStateProcessor;
 
 public class PersonCreateRequest {
     private String firstName;
@@ -12,8 +9,7 @@ public class PersonCreateRequest {
     private String email;
 
     public PersonCreateRequest(String firstName, String lastName, String phoneNumber, String email) {
-
-        determineFirstNameAndLastNameState(firstName, lastName, phoneNumber, email, (resultFirstName, resultLastName, resultPhoneNumber, resultEmail) -> {
+        NameStateProcessor.determineFirstNameAndLastNameState(firstName, lastName, phoneNumber, email, (resultFirstName, resultLastName, resultPhoneNumber, resultEmail) -> {
             this.firstName = resultFirstName;
             this.lastName = resultLastName;
             this.phoneNumber = resultPhoneNumber;
@@ -35,65 +31,5 @@ public class PersonCreateRequest {
 
     public String getEmail() {
         return email;
-    }
-
-    private void determineFirstNameAndLastNameState(
-            String firstName,
-            String lastName,
-            String phoneNumber,
-            String email,
-            PersonCallback callback) {
-
-        NameState nameState = determineNameState(firstName, lastName);
-        ContactState contactState = determineContactState(phoneNumber, email);
-
-        if (contactState == ContactState.NO_CONTACT) {
-            throw new MissingFieldException("phone number or email");
-        }
-
-        String resultFirstName = processNameState(nameState, firstName, lastName, phoneNumber, email);
-        String resultLastName = nameState == NameState.ONLY_LAST_NAME || nameState == NameState.BOTH_NAMES ? lastName : null;
-        String resultPhoneNumber = contactState == ContactState.ONLY_PHONE_NUMBER || contactState == ContactState.BOTH_CONTACT ? phoneNumber : null;
-        String resultEmail = contactState == ContactState.ONLY_EMAIL || contactState == ContactState.BOTH_CONTACT ? email : null;
-
-
-        callback.onResult(resultFirstName, resultLastName, resultPhoneNumber, resultEmail);
-    }
-
-    private String processNameState(NameState nameState, String firstName, String lastName, String phoneNumber, String email) {
-        switch (nameState) {
-            case ONLY_FIRST_NAME, BOTH_NAMES:
-                return firstName;
-            case ONLY_LAST_NAME:
-                return null; // Last name handled separately
-            case NO_NAMES:
-                return phoneNumber != null ? phoneNumber : email;
-            default:
-                throw new MissingFieldException("Unexpected nameState: " + nameState);
-        }
-    }
-
-    private NameState determineNameState(String firstName, String lastName) {
-        if (firstName != null && lastName == null) {
-            return NameState.ONLY_FIRST_NAME;
-        } else if (firstName == null && lastName != null) {
-            return NameState.ONLY_LAST_NAME;
-        } else if (firstName != null && lastName != null) {
-            return NameState.BOTH_NAMES;
-        } else {
-            return NameState.NO_NAMES;
-        }
-    }
-
-    private ContactState determineContactState(String phoneNumber, String email) {
-        if (phoneNumber != null && email == null) {
-            return ContactState.ONLY_PHONE_NUMBER;
-        } else if (phoneNumber == null && email != null) {
-            return ContactState.ONLY_EMAIL;
-        } else if (phoneNumber != null && email != null) {
-            return ContactState.BOTH_CONTACT;
-        } else {
-            return ContactState.NO_CONTACT;
-        }
     }
 }
