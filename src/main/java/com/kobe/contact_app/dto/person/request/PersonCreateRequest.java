7160112df459b735297be48c870020f200569396
Kobe <dev.skyachieve91@gmail.com>
2024-12-04 08:40:41 +0900

    Swagger에서의 각 필드 설명과 예시 작성.

--- src/main/java/com/kobe/contact_app/dto/person/request/PersonCreateRequest.java
+++ src/main/java/com/kobe/contact_app/dto/person/request/PersonCreateRequest.java
@@ -1,11 +1,19 @@
 package com.kobe.contact_app.dto.person.request;
 
 import com.kobe.contact_app.util.name.NameStateProcessor;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 public class PersonCreateRequest {
+    @Schema(description = "연락처의 이름 정보. null 값이 될 수 있습니다.", example = "민성")
     private String firstName;
+
+    @Schema(description = "연락처의 성 정보. null 값이 될 수 있습니다.", example = "강")
     private String lastName;
+
+    @Schema(description = "연락처의 전화번호 정보. 이름,성,이메일 정보가 없을 경우 null 값이 될 수 없습니다. 이메일 정보가 있을 경우 null 값이 될 수 있습니다.", example = "01012345678")
     private String phoneNumber;
+
+    @Schema(description = "연락처의 이메일 정보. 이름,성,전화번호 정보가 없을 경우 null 값이 될 수 없습니다. 전화번호 정보가 있을 경우 null 값이 될 수 있습니다.", example = "test1@example.com")
     private String email;
 
     public PersonCreateRequest(String firstName, String lastName, String phoneNumber, String email) {
