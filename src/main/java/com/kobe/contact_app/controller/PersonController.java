68b122c4a4e6c5975895c6568bb6721af4fb98dc
Kobe <dev.skyachieve91@gmail.com>
2024-12-03 09:35:58 +0900

    RequestMapping("/person") -> RequestMapping("/contact")로 수정.

--- src/main/java/com/kobe/contact_app/controller/PersonController.java
+++ src/main/java/com/kobe/contact_app/controller/PersonController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/person")
+@RequestMapping("/contact")
 public class PersonController {
     private final PersonService personService;
 
