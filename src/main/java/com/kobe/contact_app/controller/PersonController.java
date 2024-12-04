1fafa5f04be02e6d9a2b5601e33c18b0e4dd28f4
Kobe <dev.skyachieve91@gmail.com>
2024-12-04 10:11:05 +0900

    Swagger에 보여질 summary, description 작성.

--- src/main/java/com/kobe/contact_app/controller/PersonController.java
+++ src/main/java/com/kobe/contact_app/controller/PersonController.java
@@ -11,12 +11,14 @@ import com.kobe.contact_app.dto.person.response.PersonDeletePhoneNumberResponse;
 import com.kobe.contact_app.dto.person.response.PersonResponse;
 import com.kobe.contact_app.service.person.PersonService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/contact")
+@Tag(name = "Person", description = "Person API")
 public class PersonController {
     private final PersonService personService;
 
@@ -31,66 +33,79 @@ public class PersonController {
     }
 
     @GetMapping("/searchAll")
+    @Operation(summary = "Show Contact List", description = "저장되어 있는 연락처 리스트를 전부 보여줍니다.")
     public List<PersonResponse> getAllContactList() {
         return personService.getAllContactList();
     }
 
     @GetMapping("/search")
+    @Operation(summary = "Shows search results by last name or first name", description = "저장되이 있는 연락처 리스트를 성 또는 이름으로 검색한 결과로 보여줍니다.")
     public List<PersonResponse> getPeopleByName(@RequestParam("name") String name) {
         return personService.getPeopleByName(name);
     }
 
     @GetMapping("/search/phone")
+    @Operation(summary = "Shows search results by phone number", description = "저장되어 있는 연락처 리스트를 전화번호로 검색한 결과로 보여줍니다.")
     public List<PersonResponse> getPeopleByPhoneNumber(@RequestParam("number") String phoneNumber) {
         return personService.getPeopleByPhoneNumber(phoneNumber);
     }
 
     @GetMapping("/search/email")
+    @Operation(summary = "Shows search results by email", description = "저장되어 있는 연락처 리스트를 이메일로 검색한 결과로 보여줍니다.")
     public List<PersonResponse> getPeopleByEmail(@RequestParam("email") String email) {
         return personService.getPeopleByEmail(email);
     }
 
     @GetMapping("/search/english")
+    @Operation(summary = "Shows search results by english name", description = "저장되어 있는 연락처 리스트를 영어 성 또는 영어 이름으로 검색한 결과로 보여줍니다.")
     public List<PersonResponse> getPeopleByAlphabet(@RequestParam("name") String name) {
         return personService.getPeopleByAlphabet(name);
     }
 
     @PutMapping("/update/number")
+    @Operation(summary = "Edit/Update phone number", description = "전화번호를 수정합니다.")
     public PersonResponse updatePhoneNumber(@RequestBody PersonPhoneNumberUpdateRequest request) {
         return personService.updateByPhoneNumber(request.getId(), request.getPhoneNumber());
     }
 
     @PutMapping("/update/email")
+    @Operation(summary = "Edit/Update email", description = "이메일을 수정합니다.")
     public PersonResponse updateEmail(@RequestBody PersonEmailUpdateRequest request) {
         return personService.updateByEmail(request.getId(), request.getEmail());
     }
 
     @PutMapping("/update/firstname")
+    @Operation(summary = "Edit/Update first name", description = "이름을 수정합니다.")
     public PersonResponse updateFirstName(@RequestBody PersonFirstNameUpdateRequest request) {
         return personService.updateByFirstName(request.getId(), request.getFirstName());
     }
 
     @PutMapping("/update/lastname")
+    @Operation(summary = "Edit/Update last name", description = "성을 수정합니다.")
     public PersonResponse updateLastName(@RequestBody PersonLastNameUpdateRequest request) {
         return personService.updateByLastName(request.getId(), request.getLastName());
     }
 
     @PutMapping("/delete/number")
+    @Operation(summary = "Delete phone number", description = "전화번호를 삭제합니다.")
     public PersonDeletePhoneNumberResponse deletePhoneNumber(@RequestBody PersonPhoneNumberDeleteRequest request) {
         return personService.deletePhoneNumber(request.getId());
     }
 
     @PutMapping("/delete/email")
+    @Operation(summary = "Delete email", description = "이메일을 삭제합니다.")
     public PersonDeleteEmailResponse deleteEmail(@RequestBody PersonEmailDeleteRequest request) {
         return personService.deleteEmail(request.getId());
     }
 
     @PutMapping("/delete/firstname")
+    @Operation(summary = "Delete first name", description = "이름을 삭제합니다.")
     public FirstNameDeleteResponse deleteFirstName(@RequestBody FirstNameDeleteRequest request) {
         return personService.deleteFirstName(request.getId());
     }
 
     @PutMapping("/delete/lastname")
+    @Operation(summary = "Delete last name", description = "성을 삭제합니다.")
     public LastNameDeleteResponse deleteLastName(@RequestBody LastNameDeleteRequest request) {
         return personService.deleteLastName(request.getId());
     }
